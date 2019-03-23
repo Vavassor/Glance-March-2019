5d3355dac0be67bb304cf2fceac50bf753d5b523
Andrew Dawson <dawso.andrew@gmail.com>
2019-03-23 12:44:45 -0400

    Salts the access tokens stored in the database.

--- models/access-token.js
+++ models/access-token.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const bcrypt = require("bcrypt-nodejs");
+
 module.exports = (sequelize, DataTypes) => {
   const AccessToken = sequelize.define(
     "AccessToken",
@@ -8,6 +10,11 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+        validate: {
+          hashesMatch: function(value) {
+            return bcrypt.compareSync(value, this.value);
+          },
+        },
       },
       userId: {
         type: DataTypes.STRING,
@@ -16,6 +23,14 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       underscored: true,
+      hooks: {
+        beforeCreate: (accessToken) => {
+          accessToken = bcrypt.hashSync(
+            accessToken.value,
+            bcrypt.genSaltSync(10),
+            null);
+        },
+      },
     });
 
   return AccessToken;
